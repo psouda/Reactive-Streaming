45740ad8232b8cde16e38829fab0ba9b12e5e847
PAYAM SOUDACHI <payams@energizeglobal.com>
2023-06-08 16:55:44 +0400

    Add spring boot main class

---
+++ data-harvester/src/main/java/io/sample/dataharvester/DataHarvesterApplication.java
@@ -0,0 +1,11 @@
+package io.sample.dataharvester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataHarvesterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DataHarvesterApplication.class, args);
+    }
+}
